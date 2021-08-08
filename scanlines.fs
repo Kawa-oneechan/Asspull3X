752b69eef87fea24b1e6ed96d21560fc884d6b1b
Kawa <kawaoneechan@gmail.com>
2021-08-08 17:29:10 +0200

    For some reason scanlines.fs and pal-r57shell.fs broke
    In different ways no less. One gave compile errors, and the other just outright crashed to desktop.

--- scanlines.fs
+++ scanlines.fs
@@ -7,8 +7,7 @@ varying vec2 v_texCoord;
 void main()
 {
 	vec4 color = texture2D(tex0, v_texCoord);
-	int y = floor(v_texCoord.y * 480);
-	if(y % 2 == 0)
-		color *= 0.75;
-	gl_FragColor = color;
+	float y = floor(v_texCoord.y * 480);
+	bool isEven = mod(y, 2.0) == 0.0;
+	gl_FragColor = isEven ? color : (color * 0.75);
 }
