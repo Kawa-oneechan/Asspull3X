ed061251f76bffdd444c22a4035530b42490d244
Kawa <kawaoneechan@gmail.com>
2023-12-02 14:04:33 +0100

    Add floating point support
    Somehow this had to include a stupid two-line hack to enable two missing opcodes.
    
    I'm told these two opcodes are part of the 68881 coprocessor. GCC insists on using it.
    
    Fuck it. There's probably some intricate differences but THIS WORKS. Multiplying a float works.
    
    It's bullshit and I HATE IT.

--- musashi/m68kconf.h
+++ musashi/m68kconf.h
@@ -67,6 +67,7 @@
 #define M68K_EMULATE_010            OPT_ON
 #define M68K_EMULATE_EC020          OPT_OFF
 #define M68K_EMULATE_020            OPT_ON
+#define M68K_EMULATE_030            OPT_ON
 #define M68K_EMULATE_040            OPT_ON
 
 
@@ -183,6 +184,9 @@
 #define M68K_LOG_1010_1111          OPT_OFF
 #define M68K_LOG_FILEHANDLE         some_file_handle
 
+/* Emulate PMMU : if you enable this, there will be a test to see if the current chip has some enabled pmmu added to every memory access,
+ * so enable this only if it's useful */
+#define M68K_EMULATE_PMMU   OPT_OFF
 
 /* ----------------------------- COMPATIBILITY ---------------------------- */
 
